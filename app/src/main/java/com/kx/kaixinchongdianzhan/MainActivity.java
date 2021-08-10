61d0f1df3428b1320974e1c305cc77f55bc34e62
wanglizheng <wanglizheng@inke.cn>
2021-08-10 22:33:37 +0800

    全屏

--- app/src/main/java/com/kx/kaixinchongdianzhan/MainActivity.java
+++ app/src/main/java/com/kx/kaixinchongdianzhan/MainActivity.java
@@ -3,12 +3,19 @@ package com.kx.kaixinchongdianzhan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //去除状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
