dd1caf0f586b4bb8aaca3fb8bfb90cc5874fa456
wanglizheng <wanglizheng@inke.cn>
2021-08-11 13:13:45 +0800

    面板切换成功

--- app/src/main/java/com/kx/kaixinchongdianzhan/MainActivity.java
+++ app/src/main/java/com/kx/kaixinchongdianzhan/MainActivity.java
@@ -7,8 +7,12 @@ import android.text.Layout;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnLogin;
+    private Button btnRegister;
+    private boolean showLoginView = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,47 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         initLayout();
+        initButtons();
     }
 
     private void initLayout(){
+        chgViewLoginOrRegister();
+    }
+
+    private void chgViewLoginOrRegister(){
         View registerView = findViewById(R.id.layout_register);
-        registerView.bringToFront();
         View loginView = findViewById(R.id.layout_login);
-        loginView.setVisibility(View.GONE);
+        if(showLoginView){
+            loginView.setVisibility(View.VISIBLE);
+            loginView.bringToFront();
+            registerView.setVisibility(View.GONE);
+        }else {
+            loginView.setVisibility(View.GONE);
+            registerView.setVisibility(View.VISIBLE);
+            registerView.bringToFront();
+        }
+    }
+
+    private void initButtons(){
+        btnLogin = (Button)findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(this);
+        btnRegister = (Button)findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_login:
+                showLoginView = true;
+                chgViewLoginOrRegister();
+                break;
+            case R.id.btn_register:
+                showLoginView = false;
+                chgViewLoginOrRegister();
+                break;
+            default:
+                break;
+        }
     }
 }
