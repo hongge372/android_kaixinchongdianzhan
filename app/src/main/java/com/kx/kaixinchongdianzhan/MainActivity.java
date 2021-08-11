cef04c88b55a990a3366299077647f9fa7192369
wanglizheng <wanglizheng@inke.cn>
2021-08-11 11:46:21 +0800

    edit text 显示成功

--- app/src/main/java/com/kx/kaixinchongdianzhan/MainActivity.java
+++ app/src/main/java/com/kx/kaixinchongdianzhan/MainActivity.java
@@ -11,9 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        //去除状态栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
