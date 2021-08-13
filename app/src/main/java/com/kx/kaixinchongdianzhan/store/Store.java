c5c1056749a03587f85cbb4ddf148c241c4bb43d
wanglizheng <wanglizheng@inke.cn>
2021-08-13 16:20:29 +0800

    准备开发移动支付

--- app/src/main/java/com/kx/kaixinchongdianzhan/store/Store.java
+++ app/src/main/java/com/kx/kaixinchongdianzhan/store/Store.java
@@ -1,4 +1,23 @@
 package com.kx.kaixinchongdianzhan.store;
 
+import android.content.Context;
+import android.icu.number.CompactNotation;
+import android.widget.LinearLayout;
+
 public class Store {
+    private LinearLayout parentLayout;
+    private Context mainContext;
+
+    public Store(Context context, LinearLayout parent){
+        mainContext = context;
+        parentLayout = parent;
+    }
+
+    public void showEquipage(){
+        webPay();
+    }
+
+    public void webPay(){
+
+    }
 }
