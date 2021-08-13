556fce6431719ae884e8f422c747c3b8a8c4799a
wanglizheng <wanglizheng@inke.cn>
2021-08-13 18:44:46 +0800

    访问h5成功

--- app/src/main/java/com/kx/kaixinchongdianzhan/gamecontrol/pintu/characters/GamingActivity.java
+++ app/src/main/java/com/kx/kaixinchongdianzhan/gamecontrol/pintu/characters/GamingActivity.java
@@ -1,8 +1,11 @@
 package com.kx.kaixinchongdianzhan.gamecontrol.pintu.characters;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
@@ -14,6 +17,7 @@ import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.kx.kaixinchongdianzhan.R;
 import com.kx.kaixinchongdianzhan.store.Store;
@@ -23,6 +27,13 @@ public class GamingActivity extends AppCompatActivity implements View.OnClickLis
     private Button btnStore;
     private Button btnMe;
 
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_NETWORK_STATE};
+    private static int REQUEST_PERMISSION_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +42,7 @@ public class GamingActivity extends AppCompatActivity implements View.OnClickLis
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main_pager);
         initView();
+        permissionRequest();
     }
 
     private void initView(){
@@ -44,18 +56,25 @@ public class GamingActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View view) {
+        LinearLayout parent = findViewById(R.id.layout_characters_game);
+        parent.setBackgroundColor(Color.YELLOW);
         switch (view.getId()){
             case R.id.btn_gaming_character:
-                LinearLayout parent = findViewById(R.id.layout_characters_game);
-                parent.setBackgroundColor(Color.YELLOW);
                 CharactersGameSlider slider = new CharactersGameSlider(getApplicationContext(), parent);
                 slider.showView();
                 break;
             case R.id.btn_store:
-                    Store store = new Store();
+                    Store store = new Store(getApplicationContext(), parent);
+                    store.showEquipage();
                 break;
             case R.id.btn_me:
                 break;
+        }
+    }
+
+    private void permissionRequest(){
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
         }
     }
 }
--- app/src/main/java/com/kx/kaixinchongdianzhan/store/Store.java
+++ app/src/main/java/com/kx/kaixinchongdianzhan/store/Store.java
@@ -2,9 +2,17 @@ package com.kx.kaixinchongdianzhan.store;
 
 import android.content.Context;
 import android.icu.number.CompactNotation;
+import android.os.Environment;
+import android.util.Log;
+import android.webkit.WebView;
 import android.widget.LinearLayout;
 
+import com.kx.kaixinchongdianzhan.MainActivity;
+
+import java.io.File;
+
 public class Store {
+    private String tag = getClass().getName();
     private LinearLayout parentLayout;
     private Context mainContext;
 
@@ -18,6 +26,21 @@ public class Store {
     }
 
     public void webPay(){
+        WebView webView = new WebView(mainContext);
+        parentLayout.addView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        String webUrl ="http://192.168.27.76:8080/h5/pay.html";
+        //使用http协议，需要在manifest里面加上：
+        // android:usesCleartextTraffic="true"
+        webView.loadUrl(webUrl);
 
+//        File story = Environment.getExternalStorageDirectory();
+//        String htmlPath = story.toString() + "/h5/pay.html";
+//        Log.e(tag, "h5 path: " + htmlPath);
+//        webView.getSettings().setAllowFileAccess(true);
+//        webView.getSettings().setAllowContentAccess(true);
+//        webView.loadUrl("file:///" + htmlPath);
+        //test--->
+        //webView.loadUrl("http://www.baidu.com/");
     }
 }
